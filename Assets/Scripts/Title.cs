5568f93709c431337985c1d6b277cce94cccd986
famascpp <famascpp@gmail.com>
2014-02-10 21:30:47 +0900

    タイトルシーン等

---
+++ Assets/Scripts/Title.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Title : MonoBehaviour {
+
+	public Texture logo;
+
+		float x, w, y, h;
+	
+
+	void Start () {
+
+		x = Screen.width/2-logo.width/2;
+		w = logo.width;
+		y = Screen.height/2-logo.height/2-100;
+		h = logo.height;
+	}
+
+	// タイトルロゴを動かす.
+	private IEnumerator MoveLogo () {
+		yield return 0;
+	}
+
+	void OnGUI () {
+		// タイトルロゴ貼り付け.
+		if(Event.current.type == EventType.Repaint){
+			Graphics.DrawTexture(
+				new Rect(x, y,
+			         w, h),
+				logo);
+		}
+	}
+}
