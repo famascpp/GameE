da07261d12be4737c56eb1c257a94664e3e5894b
oojimakazuwo <oojimakazuwo@gmail.com>
2014-02-10 17:31:11 +0900

    music追加

---
+++ Assets/Scripts/MusicText.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
