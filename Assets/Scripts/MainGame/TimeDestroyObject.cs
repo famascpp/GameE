ee1b4dfa138f4650ee1a970dde7bdf759fa5a2b3
oojimakazuwo <oojimakazuwo@gmail.com>
2014-02-15 03:25:38 +0900

    うまくうｐできなかったのでもう一度
    内容は下と一緒

---
+++ Assets/Scripts/MainGame/TimeDestroyObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeDestroyObject : MonoBehaviour {
+
+	public float time = 5.0f;
+
+	float startTime = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( Time.time - startTime > time )
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}
