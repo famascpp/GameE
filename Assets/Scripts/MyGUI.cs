0466e20bbf10f4ce1f99f5349e1239daa5962fb5
oojimakazuwo <oojimakazuwo@gmail.com>
2014-02-22 02:58:18 +0900

    画像関係の処理を追加

---
+++ Assets/Scripts/MyGUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class MyGUI {
+
+	//高さに合わせて画像を拡縮,xとyは0~1,サイズも0~1.
+	public static void DrawTexture( Rect rect , Texture tex )
+	{
+		GUI.DrawTexture( RectHeightZeroToOne(rect) , tex );
+	}
+
+
+	public static Rect RectHeightZeroToOne(Rect rect)
+	{
+
+		rect.x = Screen.width / 2.0f + Screen.height * ( rect.x - rect.width / 2.0f  );
+		rect.y = Screen.height / 2.0f + Screen.height * ( rect.y - rect.height / 2.0f );
+		rect.height = Screen.height * ( rect.height );
+		rect.width = Screen.height * ( rect.width );
+
+		return rect;
+	}
+
+}
